bdcf0f55f255af7e0e1b74ff70336257a5eb95ce
anirban <anirbandhara1997@gmail.com>
2025-01-05 13:55:58 +0530

    Codes are commited

---
+++ src/ex_01_Java_Basic/Lab003_Print_vs_Println.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab003_Print_vs_Println {
+    public static void main(String[] args){
+        System.out.println("Line1");
+        System.out.println("Line2");
+
+        System.out.print("Line3");
+        System.out.print("Line4");
+    }
+
+}
---
+++ src/ex_01_Java_Basic/Lab004_Comment_SingleLine_vs_MultiLine.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comment_SingleLine_vs_MultiLine {
+    // This is Single Line Comment
+
+
+    /*
+     * This is Multiline Comment
+     * Author: Anirban
+     * Course: ATB10x
+     * subject: Java
+     */
+}
